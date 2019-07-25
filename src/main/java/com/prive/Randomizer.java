8c7286118fa67a7173fd22351f8b28174326a866
raj <torajakumar@gmail.com>
2019-07-25 21:16:04 +0800

    simple tokenizer

--- src/main/java/com/prive/Randomizer.java
+++ src/main/java/com/prive/Randomizer.java
@@ -1,4 +1,45 @@
 package com.prive;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Randomizer {
+    char[] chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    char[] nums = "0123456789".toCharArray();
+
+    private Map<String, String> random = new HashMap<>();
+
+    public String randomize(String data) {
+        String ramdomData = random.get(data);
+        if (ramdomData == null) {
+            random.put(data, (ramdomData = generateRandom(data)));
+        }
+
+        return ramdomData;
+    }
+
+    private String generateRandom(String data) {
+        char[] input = data.toCharArray();
+        char[] result = new char[input.length];
+
+        for (int i = 0; i < input.length; i++) {
+            if (Character.isAlphabetic(input[i])) {
+                result[i] = pickARandomChar();
+            } else if (Character.isDigit(input[i])) {
+                result[i] = pickARandomDigit();
+            } else {
+                result[i] = input[i];
+            }
+        }
+
+        return new String(result);
+    }
+
+    private char pickARandomDigit() {
+        return nums[(int) (Math.random() * nums.length)];
+    }
+
+    private char pickARandomChar() {
+        return chars[(int) (Math.random() * nums.length)];
+    }
 }
--- src/main/java/com/prive/Tokenizer.java
+++ src/main/java/com/prive/Tokenizer.java
@@ -1,10 +1,44 @@
 package com.prive;
 
-/**
- * Hello world!
- */
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
 public class Tokenizer {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private Randomizer randomizer = new Randomizer();
+
+    public static void main(String[] args) throws IOException {
+        String inputFile = (args.length > 0) ? args[0] : "data.txt";
+        String delimiter = (args.length > 1) ? args[1] : "\\|";
+        int column = (args.length > 2) ? Integer.valueOf(args[2]) : 0;
+
+        new Tokenizer().scramble(inputFile, delimiter, column);
+    }
+
+    public void scramble(String inputFile, String delimiter, int columnToScramble) throws IOException {
+        Files.readAllLines(Paths.get(inputFile), Charset.defaultCharset())
+                .stream()
+                .map(line -> tokenize(line, delimiter))
+                .map(tokenz -> scramble(tokenz, columnToScramble))
+                .forEach(a -> System.out.println(Arrays.asList(a)));
+    }
+
+    private String[] scramble(String[] tokenz, int columnToScramble) {
+        String[] scrambledData = new String[tokenz.length];
+        for (int i = 0; i < tokenz.length; i++) {
+            if (i == columnToScramble) {
+                scrambledData[i] = randomizer.randomize(tokenz[i]);
+            } else {
+                scrambledData[i] = tokenz[i];
+            }
+        }
+
+        return scrambledData;
+    }
+
+    private String[] tokenize(String line, String delimiter) {
+        return line.split(delimiter);
     }
 }
