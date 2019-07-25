25464546ad97dd77feb912bbbfe737936560f45a
raj <torajakumar@gmail.com>
2019-07-25 20:37:59 +0800

    initialize

---
+++ src/main/java/com/prive/Tokenizer.java
@@ -0,0 +1,10 @@
+package com.prive;
+
+/**
+ * Hello world!
+ */
+public class Tokenizer {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/prive/TokenizerTest.java
@@ -0,0 +1,18 @@
+package com.prive;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple Tokenizer.
+ */
+public class TokenizerTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
